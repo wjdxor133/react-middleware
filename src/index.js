58bdb68b28fe2dc42d39ff1350b34853d17c2bb8
wjdxor133 <mahan89@daum.net>
2020-08-22 20:44:32 +0900

    미들웨어 실습해보기

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import rootReducer from "./moudles";
+import loggerMiddleware from "./loggerMiddleare/loggerMiddleware";
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
